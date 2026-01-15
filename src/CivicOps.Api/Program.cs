8dfaa152bcdae86708dc31c5cafa4aa8e9b441b8
Joel Southall <vextechmage@gmail.com>
2026-01-15 02:55:46 -0800

    Add stub /api/me endpoint and serialize enums as strings

--- src/CivicOps.Api/Program.cs
+++ src/CivicOps.Api/Program.cs
@@ -1,6 +1,34 @@
+using CivicOps.Contracts.Auth;
+using CivicOps.Contracts.Users;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+
+var api = app.MapGroup("/api");
+
+api.MapGet("/me", () =>
+{
+    var me = new MeResponse(
+        Id: Guid.Parse("11111111-1111-1111-1111-111111111111"),
+        Email: "dev@civicops.local",
+        DisplayName: "Dev User",
+        Role: Role.Admin,
+        IsActive: true
+    );
+
+    return Results.Ok(me);
+});
+
+app.MapGet("/", () => Results.Ok("CivicOps API is running. Try GET /api/me"));
+
 
 app.Run();
